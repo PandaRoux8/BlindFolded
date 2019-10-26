7fe43df2dead9b5163fe2ab5ca1a86de58948f9c
Jules <jules.schluchter@gmail.com>
2019-10-26 16:02:34 +0200

    update venv

--- gamePackage/__main__.py
+++ gamePackage/__main__.py
@@ -6,14 +6,10 @@ if __name__ == "__main__":
     pygame.init()
     # Set title of the window
     pygame.display.set_caption('Blindfolded')
-    # TODO : get this from a file
     # Screen size
-    # TODO : We'll need to read a save file to know the user conf
     width = 832
     height = 832
     # Init the screen size
     screen = pygame.display.set_mode((width, height))
-    # Frame rate (clock tick in the while true of the game)
-    framerate = 15
     clock = pygame.time.Clock()
     main_menu = main_menu.MainMenu(screen, (width, height))
